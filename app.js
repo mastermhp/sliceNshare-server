faf4bfb17b57cedade3ad6062ee08abe5278d083
MEHEDI HASAN PARASH <112463517+mastermhp@users.noreply.github.com>
2025-05-18 23:00:27 +0600

    url changed

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get('/api/v1', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>API v1 Response test</title>
+      <title>API v1 Response testing</title>
       <style>
         body {
           font-family: Arial, sans-serif;
